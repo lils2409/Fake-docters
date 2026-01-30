e9d59588ba231a36ec9315575000c0cdcd8c54ee
Thanapan Chitlak <thanachi@thanachys-MacBook-Air.local>
2026-01-31 01:18:24 +0700

    editfilenames

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = "fake_doctors_secure_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
+db = SQLAlchemy(app)
