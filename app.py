3ae63d351586860430947459b3784fb867aa7ca2
Thanapan Chitlak <thanachi@thanachys-MacBook-Air.local>
2026-02-17 08:05:13 +0700

    app

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     patient_id = db.Column(db.String(20), unique=True, nullable=False)
 
-
     name = db.Column(db.String(100))
     surname = db.Column(db.String(100))
     sex = db.Column(db.String(10))
